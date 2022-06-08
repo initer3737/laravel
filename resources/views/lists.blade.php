9a2c983b3a41e41315a689e7f844dfe5d5053537
initer3737 <puthutwahyuaji13@gmail.com>
2022-06-08 13:15:43 +0700

    minor change

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -1,24 +1,38 @@
 @extends('template.template')
 @section('title','list')
 @section('main')
-    <div class="modal fade" id="status">
+    <div class="modal fade" id="status" data-bs-backdrop="static" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header"></div>
+                <div class="modal-header">
+                    <h5 class="modal-title">post status</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                </div>
                 <div class="modal-body">
                     <form action="/api/list/add" method="post" class="container rounded d-flex flex-column gap-3 mt-3 p-4 rounded shadow" enctype="multipart/form-data">
                         @csrf
                         <div class="form-floating">
                         <input type="text" class="form-control rounded shadow py-3" id="input" placeholder="my todos" required name="todo">
+                        @error('todo')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
                         <label for="input">my todos</label>
                         </div>
-                        <label for="file"><p><i class="h2 bi bi-arrow-up-circle-fill upload-link"> upload</i></p></label>
+                        <label for="file"><p class="mx-auto"><i class="h2 bi bi-arrow-up-circle-fill upload-link"> upload</i></p></label>
                         <input type="file" class="form-control shadow rounded d-none py-3" id="file" required name="file" onchange="Preview()">
+                        @error('file')
+                        <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
                         <img alt="" srcset="" class="img-fluid w-50" id="img-preview">
                         <div class="form-floating">
                         <input type="text" class="form-control rounded shadow py-3" id="filename" placeholder="filename" required name="filename">
                         <label for="filename">filename</label>
                         </div>
+                        @error('filename')
+                            <div class="alert alert-danger">{{$message}}</div>
+                        @enderror
                         <button class="btn btn-outline-secondary py-2 px-5 rounded shadow text-capitalize" name="send"><i class="bi bi-bookmark-plus-fill"></i> send</button>
                     </form>
                 </div>
@@ -26,7 +40,7 @@
             </div>
         </div>
     </div>
-        <div class="col-12 mt-5">
+        <div class="col-12 mt-5 shadow">
             @error('todo')
             <div class="alert alert-danger">{{$message}}</div>
             @enderror
@@ -65,6 +79,7 @@
     {{-- axios library --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.min.js" integrity="sha512-xIPqqrfvUAc/Cspuj7Bq0UtHNo/5qkdyngx6Vwt+tmbvTLDszzXM0G6c91LXmGrRx8KEPulT+AfOOez+TeVylg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
+
      let url="http://127.0.0.1:8000/api/list";
        
     const get=url=>{
