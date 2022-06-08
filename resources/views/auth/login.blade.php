9287b6c732cb2f3dce4a5f60ccf33dfe025579a4
initer3737 <puthutwahyuaji13@gmail.com>
2022-06-08 21:32:32 +0700

    username

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,10 @@
       <label for="floatingInput">Email address</label>
     </div>
     <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">username</label>
+    </div>
+    <div class="form-floating">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
